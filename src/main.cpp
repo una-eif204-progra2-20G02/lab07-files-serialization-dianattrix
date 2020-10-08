957db625c9c76cbaa17d77dc3abc0ef2f9441d86
Diana Fallas <65523878+dianattrix@users.noreply.github.com>
2020-10-07 20:35:08 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,12 @@ using namespace std;
 #include"Person.h"
 #include"SerializationManager.h"
 #include"JsonSerialization.h"
+#include"BinaryFile.h"
 
 int main() {
+
+    ISerialization *jsonSerialization = new JsonSerialization();
+    BinaryFile* binaryFile = new BinaryFile();
 
     Person person1("Diana", 20, 117770654);
     Person person2("Carlos", 31, 123456789);
@@ -22,11 +26,8 @@ int main() {
     personList.addPerson(person3);
 
     SerializationManager serializationManager;
-    serializationManager.savePersonBinaryFile(person1);
-    serializationManager.savePersonBinaryFile(person2);
-    serializationManager.savePersonBinaryFile(person3);
+    serializationManager.saveBinaryFile(personList, binaryFile);
 
-    ISerialization *jsonSerialization = new JsonSerialization();
     serializationManager.serialize(personList, jsonSerialization);
     serializationManager.deserialize(personList, jsonSerialization);
 
